611bb32d86267c921249b0253dd8b1f55b0f4db3
Dave Foley <davidmfoley@gmail.com>
2011-04-19 21:13:25 -0700

    a little less shitty, again

--- lib/analdiffist/diff_set.rb
+++ lib/analdiffist/diff_set.rb
@@ -22,23 +22,25 @@ module AnalDiffist
         keys = (before_by_context.keys + after_by_context.keys).uniq
         to_return = []
         keys.each do |key| #=> [Reek, 'Class#method']
-          before_problems = before_by_context[key] || [] 
+          before_problems = before_by_context[key] || []
           after_problems = after_by_context[key] || []
 
-          before_len = before_problems.length
-          after_len = after_problems.length
-
-          0.upto([before_len, after_len].min - 1) do |i|
-            to_return << after_problems[i].diff(before_problems[i])
-          end
-
-          to_return = to_return.concat( compare_difference_lists(before_problems, after_problems))
-          removed =  compare_difference_lists(after_problems, before_problems)
-          to_return = to_return.concat(removed.map{|diff| InvertedDiff.new(diff)})
-
+          to_return = to_return.concat(compare_problem_sets(before_problems, after_problems))
         end
         to_return.reject(&:nil?)
       end
+    end
+
+    def compare_problem_sets(before_problems, after_problems)
+      count_in_both = [before_problems.length,after_problems.length].min
+      diffs = []
+      0.upto(count_in_both - 1) do |i|
+        diffs << after_problems[i].diff(before_problems[i])
+      end
+
+      added =  compare_difference_lists(before_problems, after_problems)
+      removed =  compare_difference_lists(after_problems, before_problems).map{|diff| InvertedDiff.new(diff)}
+      diffs + added + removed
     end
 
     def compare_difference_lists a, b
@@ -47,17 +49,6 @@ module AnalDiffist
         to_return << b[i].diff(nil)
       end
       to_return
-    end
-
-    def compare(a,b)
-      #TODO: move this comparison into the class?
-      a.map do |problem| 
-
-        matching_problem = b.detect {|problem2| [problem.type, problem.context] == [problem2.type, problem2.context] }
-
-          problem.diff(matching_problem)
-
-      end.reject {|x| x.nil?}
     end
   end
 
