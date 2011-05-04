4a9f4ff0835c2c99e9aad62ec7f27b9fbe1a630f
Dave Foley <davidmfoley@gmail.com>
2011-05-04 07:53:58 -0700

    rspec cleanup

--- spec/lib/analdiffist/diff_set_spec.rb
+++ spec/lib/analdiffist/diff_set_spec.rb
@@ -1,9 +1,7 @@
 require  'spec_helper'
+
 RSpec::Matchers.define :have_a_single_problem do |type, context|
   match do |problems|
-    puts type
-    puts context
-    puts problems.first.inspect
     problems.length.should == 1
     problems.first.type.should == type
     problems.first.context.should == context
@@ -12,75 +10,49 @@ end
 
 describe 'diffing two files' do
   context 'single problem added' do
-    before do
-      @diff = AnalDiffist::DiffSet.new([], [test_problem('foo', 'bar')])
-    end
-    it 'should find a new reek' do
-      @diff.added_problems.should have_a_single_problem('foo', 'bar')
-    end
-    it 'should not have any removed reeks' do
-      @diff.removed_problems.should == []
-    end
+    subject{AnalDiffist::DiffSet.new([], [test_problem('foo', 'bar')])}
+
+    its(:removed_problems) {should == []}
+    its(:added_problems) {should  have_a_single_problem 'foo', 'bar'}
   end
 
   context 'single problem removed' do
-    before do
-      @diff = AnalDiffist::DiffSet.new( [test_problem('foo', 'bar')], [])
-    end
+    subject { AnalDiffist::DiffSet.new( [test_problem('foo', 'bar')], [])}
 
-    it 'should find a removed reek' do
-      @diff.removed_problems.should have_a_single_problem 'foo', 'bar'
-    end
-
-    it 'should not have any added reeks' do
-      @diff.added_problems.should == []
-    end
+    its(:removed_problems) {should have_a_single_problem 'foo', 'bar' }
+    its(:added_problems) {should == []}
   end
 
   context 'one removed, one added, one remains' do
-    before do
+    subject do
       before = [test_problem('foo', 'bar'), test_problem('removed', 'removed')]
       after = [test_problem('foo', 'bar'), test_problem('added', 'added')]
-      @diff = AnalDiffist::DiffSet.new(before, after)
+      AnalDiffist::DiffSet.new(before, after)
     end
 
-    it 'should find a removed reek' do
-      @diff.removed_problems.should have_a_single_problem 'removed', 'removed'
-    end
-
-    it 'should find an added problem' do
-      @diff.added_problems.should have_a_single_problem 'added', 'added'
-    end
+    its(:removed_problems) {should  have_a_single_problem 'removed', 'removed'}
+    its(:added_problems) {should have_a_single_problem 'added', 'added'}
   end
 
   context 'two reeks with same context and type, then one is removed' do
-    before do
+    subject do
       before = [test_problem('same-type', 'bar'), test_problem('same-type', 'bar')]
       after = [test_problem('same-type', 'bar')]
-      @diff = AnalDiffist::DiffSet.new(before, after)
+      AnalDiffist::DiffSet.new(before, after)
     end
 
-    it 'should show one removed problem' do
-      @diff.removed_problems.length.should ==1
-    end
-
-    it 'should have zero added problems' do
-      @diff.added_problems.length.should == 0
-    end
+    its(:removed_problems) {should  have_a_single_problem 'same-type', 'bar'}
+    its(:added_problems) {should == []}
   end
 
   context 'when scores change' do
-    before do
+    subject do
       before = [AnalDiffist::FlogProblem.new('bar', 17.1)]
       after = [AnalDiffist::FlogProblem.new('bar', 18.5)]
-      @diff = AnalDiffist::DiffSet.new(before, after)
+      AnalDiffist::DiffSet.new(before, after).added_problems
     end
-
-    it 'should identify as added' do
-      added_problems = @diff.added_problems
-      added_problems.length.should == 1
-      added_problems.first.score.round(1).should == 1.4
-    end
+    specify {subject.should have(1).item}
+    specify {subject.first.score.should == 1.4 }
   end
 
   def test_problem type, context, score = 1
